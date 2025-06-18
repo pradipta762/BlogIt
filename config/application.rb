eb0ebac28d6494801369ca9c07092e0fb9d1b9e2
Pradipta Dash <pradiptadash762@gmail.com>
2025-06-18 18:49:48 +0530

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative "boot"
+
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module BlogIt
+  class Application < Rails::Application
+    config.load_defaults 7.1
+    config.autoload_lib(ignore: %w(assets tasks))
+    config.generators do |g|
+      g.test_framework :test_unit, fixture: false
+    end
+  end
+end
