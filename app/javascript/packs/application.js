08efd52f268b15257ca21c28d4cb895c80e9d23b
Pradipta Dash <pradiptadash762@gmail.com>
2025-06-25 12:00:26 +0530

    Added neetoUI

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,19 @@
 import "../stylesheets/application.scss";
 import ReactRailsUJS from "react_ujs";
+import App from "../src/App";
 
 import { setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
-import App from "../src/App";
+import Logger from "js-logger";
 
 initializeLogger();
 setAuthHeaders();
 
+Logger.info(App);
+console.log("Hello");
+
 const componentsContext = { App };
 ReactRailsUJS.getConstructor = name => {
+  console.log(name);
   return componentsContext[name];
 };
