d9edef3966a5f188acc15ba825a1353703ec8b66
Pradipta Dash <pradiptadash762@gmail.com>
2025-08-01 22:40:57 +0530

    Added postgree and fixed migrations

--- db/migrate/20250626130412_seed_slug_value_for_existing_posts.rb
+++ db/migrate/20250626130412_seed_slug_value_for_existing_posts.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 class SeedSlugValueForExistingPosts < ActiveRecord::Migration[7.1]
+  class Post < ActiveRecord::Base
+    self.table_name = "posts"
+  end
+
   def up
+    Post.reset_column_information
     Post.find_each do |post|
-      post.send(:set_slug)
-      post.save!(validate: false)
+      post.update_column(:slug, post.title.parameterize)
     end
   end
 
   def down
-    Post.find_each do |post|
-      post.slug = nil
-      post.save!(validate: false)
-    end
+    Post.update_all(slug: nil)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_07_25_100602) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,8 +49,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_25_100602) do
   end
 
   create_table "categories_posts", id: false, force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "category_id", null: false
+    t.bigint "post_id", null: false
+    t.bigint "category_id", null: false
     t.index ["category_id", "post_id"], name: "index_categories_posts_on_category_id_and_post_id"
     t.index ["post_id", "category_id"], name: "index_categories_posts_on_post_id_and_category_id"
   end
@@ -67,9 +70,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_25_100602) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug", null: false
-    t.integer "user_id", null: false
-    t.integer "organization_id", null: false
     t.string "status", default: "publish", null: false
+    t.bigint "user_id", null: false
+    t.bigint "organization_id", null: false
     t.index ["organization_id"], name: "index_posts_on_organization_id"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
     t.index ["user_id"], name: "index_posts_on_user_id"
@@ -79,7 +82,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_25_100602) do
     t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
-    t.integer "organization_id", null: false
+    t.bigint "organization_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "authentication_token"
@@ -87,8 +90,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_25_100602) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.string "vote_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
